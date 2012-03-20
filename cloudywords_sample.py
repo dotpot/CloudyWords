7c57d4f0cb5a42d6693a96f7ac2559c4e1adfa43
Lukas Salkauskas <halfas.online@gmail.com>
2012-03-20 13:33:45 +0200

    comments

--- cloudywords_sample.py
+++ cloudywords_sample.py
@@ -5,12 +5,13 @@ __author__ = 'lus'
 def main():
     cw = CloudyWords()
 
-    # Add phrase.
+    # add phrase.
     cw.addWords("""
     his is a fantastic phrase, it has been popular and it has worked; testing now starts earlier and testing has a higher profile than ever before, but despite all that, it isn't what we meant or even what we really needed.
     Back in the depths of despair that was the Software Crisis. Testers managed to bellow a rallying cry "Testing should start early!" Testers could be found picketing management and development meetings with 'V-model' placards and T-shirts with 'cost of defect' bar charts emblazoned.
     """)
 
+    # add one word.
     cw.addWords('testing')
 
     res = cw.cloud()
